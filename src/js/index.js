a86c43be09beb8cbfbea8d8f62a5831be0ec96d4
Danil <makarovdanil289@gmail.com>
2023-07-27 15:57:25 +0500

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+'use strcit'
+
+let money = prompt("Ваш бюджет на месяц?", '');
+let time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+let addData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses:  {},
+    income: [],
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(budget / 30);
